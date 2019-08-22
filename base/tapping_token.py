e7d8f2c65314c6fd3d2ecb040144fb850f428258
mimipei <18216490426@163.com>
2019-08-22 14:19:43 +0800

    格式调整

--- base/tapping_token.py
+++ base/tapping_token.py
@@ -2,13 +2,12 @@ import requests
 import xlrd
 import re
 
-data = xlrd.open_workbook('D:/PycharmProjects/Fenmi/dataconfig/user.xlsx')
-tables = data.sheet_by_index(0)
-username = tables.cell(1, 0).value
-password = tables.cell(1, 1).value
-
 
 def tapping_token():
+    data = xlrd.open_workbook('D:/PycharmProjects/Fenmi/dataconfig/user.xlsx')
+    tables = data.sheet_by_index(0)
+    username = tables.cell(1, 0).value
+    password = tables.cell(1, 1).value
     data = {
         "username": username,
         "password": password,
@@ -17,10 +16,7 @@ def tapping_token():
     url = "http://test.51fenmi.com/api/user/public/login"
     response = requests.post(url, data)
     return_result = response.json()
-    login_token = re.findall(r"'token':..(.+)....beService'", 
-    str(return_result))
+    login_token = re.findall(r"'token':..(.+)....beService'",
+                             str(return_result))
     return login_token[0]
 
-
-data = tapping_token()
-print(data)
